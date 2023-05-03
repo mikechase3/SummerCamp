19dd137024ab47f8b42f035e5a9c6ed4730f0b65
mikechase3 <protectmikechase@gmail.com>
2023-05-03 20:56:32 +0000

    Tkinter message up

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Streamliner")
+
+l_streamliner = ttk.Label(root, text="Streamliner", font=("Arial 16 bold"))
+
+l_streamliner.pack()
+
+root.mainloop()
